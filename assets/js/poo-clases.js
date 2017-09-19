d0cd34ceb856d9f3049ddd85dcb7027975f8332b
jani-123 <janina.rqa@gmail.com>
2017-09-19 17:05:32 -0500

    paradigma poo-clases

--- assets/js/poo-clases.js
+++ assets/js/poo-clases.js
@@ -1,26 +1,23 @@
 class Buscaminas{
-    constructor(){
-        this.numeroMinas = 7;
-        this.matriz = [];
-    }
+    
     creaMatriz()
     {
-        this.matriz = [];
-        for (var i = 0; i < 7; i++) {
-            this.matriz[i] = [];
+        let matriz = [];
+        for (let i = 0; i < 7; i++) {
+            matriz[i] = [];
             for (var j = 0; j < 7; j++) {
-                this.matriz[i][j] = 0;
+                matriz[i][j] = 0;
             }
         }
-        return this.matriz;
+        return matriz;
     }
     creaMinas() 
     {
-        let caja = $('#tableroBuscaminas');
+        let caja = document.getElementById('tableroBuscaminas');
         caja.innerHTML = '';
-        for (var i = 0; i < 7; i++) {
-            for (var j = 0; j < 7; j++) {
-                caja.innerHTML += '<input type="button" name="btn" id="' + i + '_' + j + '" value=" "/>';
+        for (let i = 0; i < 7; i++) {
+            for (let j = 0; j < 7; j++) {
+                caja.innerHTML += '<input type="button" name="btn" id="' + i+ '_' + j + '" value=" "/>';
             }
             caja.innerHTML += '<br/>';
         }
@@ -31,18 +28,17 @@ class Buscaminas{
     }
     generaBuscanimas(maximoMinas) 
     { 
-        var campo = creaMatriz();
-        var contadordeMinas = 0;
+        let campo = this.creaMatriz();
+        let contadordeMinas = 0;
         while (contadordeMinas < maximoMinas) {
-            var minasAleatorias = Aleatorio(0, 1);
-            var posicionAleatoria = [Aleatorio(0, 7 - 1), Aleatorio(0, 7 - 1)];
-            console.log(posicionAleatoria);
+            let minasAleatorias = this.Aleatorio(0, 1);
+            let posicionAleatoria = [this.Aleatorio(0, 7 - 1), this.Aleatorio(0, 7 - 1)];
             if (!campo[posicionAleatoria[0]][posicionAleatoria[1]]) {
                 contadordeMinas += (minasAleatorias) ? 1 : 0;
                 if (minasAleatorias) {
                     campo[posicionAleatoria[0]][posicionAleatoria[1]] = '*';
-                    for (var x = posicionAleatoria[0] - 1; x <= posicionAleatoria[0] + 1; x++) {
-                        for (var y = posicionAleatoria[1] - 1; y <= posicionAleatoria[1] + 1; y++) {
+                    for (let x = posicionAleatoria[0] - 1; x <= posicionAleatoria[0] + 1; x++) {
+                        for (let y = posicionAleatoria[1] - 1; y <= posicionAleatoria[1] + 1; y++) {
                             try {
                                 campo[x][y] += (campo[x][y] != '*') ? 1 : '';
                             }
@@ -54,45 +50,34 @@ class Buscaminas{
         }
         return campo;
     }
-
-}
-
-
-
-
-
-
-
-
-
-
-function jugar() 
-{
-    var numeroMinas = 7;
-    var intentosTotales = (7 * 7) - numeroMinas;
-    creaMinas();
-    matrix = generaBuscanimas(numeroMinas);
-    buttons = document.getElementsByName('btn');
-    for (var i = 0; i < buttons.length; i++) {
-        var eventoClick = document.getElementById(buttons[i].id);
-        eventoClick.addEventListener("click", jugare, false);
-        function jugare(e) {
-            var punto = e.target.id.split('_');
-            e.target.value = matrix[parseInt(punto[0])][parseInt(punto[1])];
-            if (matrix[parseInt(punto[0])][parseInt(punto[1])] == '*') {
-                alert('Esto ha explotado!');
-                remover();
-            };
-            e.target.disabled = "true";
-            intentosTotales--;
-            if (!intentosTotales) {
-                alert('Sorprendentemente Ganastes :)');
+    jugar() 
+    {
+        let numeroMinas = 7;
+        let intentosTotales = (7 * 7) - numeroMinas;
+        this.creaMinas();
+        let matrix = this.generaBuscanimas(numeroMinas);
+        let buttons = document.getElementsByName('btn');
+        for (let i = 0; i < buttons.length; i++) {
+            let eventoClick = document.getElementById(buttons[i].id);
+            eventoClick.addEventListener("click", jugare, false);
+            function jugare(e) {
+                let punto = e.target.id.split('_');
+                e.target.value = matrix[parseInt(punto[0])][parseInt(punto[1])];
+                if (matrix[parseInt(punto[0])][parseInt(punto[1])] == '*') {
+                    alert('Esto ha explotado!');
+                    remover();
+                };
+                e.target.disabled = "true";
+                intentosTotales--;
+                if (!intentosTotales) {
+                    alert('Sorprendentemente Ganastes :)');
+                }
             }
-        }
-        function remover() {
-            eventoClick.removeEventListener("click", jugare);
+            function remover() {
+                eventoClick.removeEventListener("click", jugare);
+            }
         }
     }
 }
-
-jugar();
+let jugador = new Buscaminas();
+jugador.jugar();
